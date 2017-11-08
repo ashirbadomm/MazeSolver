2d2498455c55e8fbdde21d7bd32a3fea2b48e511
ASHIRBAD SAHU <33455630+ashirbadomm@users.noreply.github.com>
2017-11-08 08:58:06 +0530

    Initial commit

---
+++ Assets/Scripts/Autowalk.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Autowalk : MonoBehaviour {
+	public float speed = 2.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+	}
+
+
+}
